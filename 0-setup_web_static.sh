3fa2c97052cf47d43312c859523fb31d83a81304
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-08-25 20:59:51 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # set up dummy html
 
-server="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+server="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n}"
+root="\troot /data/web_static/releases/test/index.html;\n"
 file="/etc/nginx/sites-available/default"
 apt-get update -y
 apt-get install nginx -y
@@ -9,4 +10,5 @@ mkdir -p "/data/web_static/releases/test/"
 echo -e "Holberton" > "/data/web_static/releases/test/index.html"
 rm -f "/data/web_static/current"; ln -s "/data/web_static/releases/test/" "/data/web_static/current"
 # chown -h ubuntu:ubuntu "/data/"
+sed -i "24i\ $root" "$file"
 sed -i "29i\ $server" "$file"
