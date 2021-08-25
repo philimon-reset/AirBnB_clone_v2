fe59788342333083bc82aa170a8b75a8c04e45d4
philimon-reset <philimonderib2019@gmail.com>
2021-08-25 19:21:19 +0000

    puppet

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,31 @@
+# deploy static
+$whisper_dirs = [ '/data/', '/data/web_static/',
+                        '/data/web_static/releases/', '/data/web_static/shared/',
+                        '/data/web_static/releases/test/'
+                  ]
+
+package {'nginx':
+  ensure  => installed,
+}
+
+file { $whisper_dirs:
+        ensure => 'directory',
+}
+file { '/data/web_static/current':
+  ensure => link,
+  target => '/data/web_static/releases/test/',
+}
+file {'/data/web_static/releases/test/index.html':
+  ensure => present,
+  content => 'Holberton School for the win!',
+}
+
+file_line {'deploy static':
+  path => '/etc/nginx/sites-available/default',
+  after => 'server_name _;',
+  line => "\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}",
+}
+
+service {'nginx':
+  ensure  => running,
+}
