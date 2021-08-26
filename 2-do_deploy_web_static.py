53d2365d4be694496d8068c7f2ec0541a19af61b
Philimon Derib <philimonderib2019@gmail.com>
2021-08-26 22:44:26 +0300

    temp

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,12 +17,12 @@ def do_deploy(archive_path):
         base = arc[1].strip('.tgz')
         put(archive_path, '/tmp/{}'.format(arc[1]))
         sudo('mkdir -p /data/web_static/releases/{}'.format(base))
-        main = "/data/web_static/releases/{}/".format(base)
-        sudo('tar -xzf /tmp/{} -C {}'.format(arc[1], main))
+        main = "/data/web_static/releases/{}".format(base)
+        sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
         sudo('rm /tmp/{}'.format(arc[1]))
-        sudo('mv {}web_static/* {}'.format(main, main))
+        sudo('mv {}/web_static/* {}/'.format(main, main))
         sudo('rm -rf /data/web_static/current')
-        sudo('ln -s {} "/data/web_static/current"'.format(main))
+        sudo('ln -s {}/ "/data/web_static/current"'.format(main))
         return True
     except:
         return False
