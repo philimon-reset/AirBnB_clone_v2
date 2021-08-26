c48499dafa92ac1b651eefe4872f4183c69e9e8b
Philimon Derib <philimonderib2019@gmail.com>
2021-08-26 19:38:19 +0300

    test

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
-"""web server distribution
-    """
+"""web server distribution"""
 from fabric.api import *
 import os.path
+
+
+def do_pack():
+    """distributes an archive to your web servers
+    """
+    target = local("mkdir -p versions")
+    name = str(datetime.now()).replace(" ", '')
+    opt = re.sub(r'[^\w\s]', '', name)
+    tar = local('tar -cvzf versions/web_static_{}.tgz web_static'.format(opt))
+    if os.path.exists("./versions/web_static_{}.tgz".format(opt)):
+        return os.path.normpath("/versions/web_static_{}.tgz".format(opt))
+    else:
+        return None
 
 
 def do_deploy(archive_path):
     """distributes an archive to your web servers
     """
-    env.hosts = ['ubuntu@104.196.155.240', 'ubuntu@34.74.146.120']
+    env.user = 'ubuntu'
+    env.hosts = ["104.196.155.240", "34.74.146.120"]
     if os.path.exists(archive_path) is False:
         return False
     try:
