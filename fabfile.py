59cc1d9126ed9e334160c3559ba39806dd48d17d
Philimon Derib <philimonderib2019@gmail.com>
2021-08-27 00:19:51 +0300

    test

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
 from fabric.api import *
 env.user = 'ubuntu'
-env.hosts = ["104.196.155.240", "34.74.146.120"]
+env.hosts = ["104.196.155.240"]
 env.key_filename = "/home/ubuntu/.ssh/id_rsa"
 def hello():
     target = run('ls | xargs stat | grep "Change" | cut -d " " -f 2,3')
