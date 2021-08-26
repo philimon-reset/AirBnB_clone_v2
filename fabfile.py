b9b8aa75508a95b338afb5333fcd15b7dce127a1
Philimon Derib <philimonderib2019@gmail.com>
2021-08-27 01:08:17 +0300

    test

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,8 @@ env.user = 'ubuntu'
 env.hosts = ["104.196.155.240"]
 env.key_filename = "/home/ubuntu/.ssh/id_rsa"
 def hello():
-    target = local('ls -t ./versions')
-    new = local("pwd")
-    print(new)
-    print(target.split())
+    with lcd("./versions")
+        target = local('ls -t')
+        new = local("pwd")
+        print(new)
+        print(target.split())
