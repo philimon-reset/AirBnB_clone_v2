850d7501da254d951bb98285c27696e30c6ec84c
Philimon Derib <philimonderib2019@gmail.com>
2021-08-27 00:52:42 +0300

    test

--- fabfile.py
+++ fabfile.py
@@ -3,8 +3,5 @@ env.user = 'ubuntu'
 env.hosts = ["104.196.155.240"]
 env.key_filename = "/home/ubuntu/.ssh/id_rsa"
 def hello():
-    target = run('cd AirBnB_Clone_V2/versions')
-    t = run('pwd')
-    print(t)
-    target = run('ls | xargs stat | grep "Change" | cut -d " " -f 2,3 > ../text')
-    
+    target = run('ls -t ./AirBnB_Clone_V2/versions')
+    print(target)
