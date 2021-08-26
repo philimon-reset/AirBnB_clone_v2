0c92c73a962478f7543709ca98c45c37166755f2
Philimon Derib <philimonderib2019@gmail.com>
2021-08-26 23:02:05 +0300

    tar

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,8 +8,9 @@ import re
 from datetime import datetime
 
 env.user = 'ubuntu'
-env.hosts = ['ubuntu@104.196.155.240', 'ubuntu@34.74.146.120']
-env.key_filename = '~/.ssh/id_rsa'
+env.hosts = ["104.196.155.240", "34.74.146.120"]
+env.key_filename = "/home/ubuntu/.ssh/id_rsa"
+
 
 
 def do_pack():
@@ -31,14 +32,16 @@ def do_deploy(archive_path):
     if os.path.exists(archive_path) is False:
         return False
     try:
-        base = archive_path.strip('.tgz')
-        put(archive_path, '/tmp')
-        run('mkdir -p /data/web_static/releases/{}'.format(base))
-        main = "/data/web_static/releases/{}/".format(base)
-        run('tar -xzf /tmp/{} -C {}'.format(archive_path, main))
-        run('rm /tmp/{}'.format(archive_path))
-        run('rm -rf /data/web_static/current')
-        run('ln -s  {} "/data/web_static/current"'.format(main))
+        arc = archive_path.split("/")
+        base = arc[1].strip('.tgz')
+        put(archive_path, '/tmp/')
+        sudo('mkdir -p /data/web_static/releases/{}'.format(base))
+        main = "/data/web_static/releases/{}".format(base)
+        sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
+        sudo('rm /tmp/{}'.format(arc[1]))
+        sudo('mv {}/web_static/* {}/'.format(main, main))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {}/ "/data/web_static/current"'.format(main))
         return True
     except:
         return False
