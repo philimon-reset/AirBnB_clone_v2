0415641d47bd7370bfe0cde1140cb3d6561f5360
Philimon Derib <philimonderib2019@gmail.com>
2021-08-26 13:24:10 +0300

    fab

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,11 +8,12 @@ def do_pack():
         target = local("mkdir -p version")
         name = str(datetime.now()).replace(" ", '')
         opt = re.sub(r'[^\w\s]','', name)
-        with tarfile.open("./version/web_static_{}".format(opt), "w:gz") as tar:
+        with tarfile.open("./version/web_static_{}.tgz".format(opt), "w:gz") as tar:
                 tar.add('web_static')
-        t = tarfile.open("./version/web_static_{}".format(opt), 'r')
-        for member in t.getmembers():
-                print(member.name)
+        if os.path.exists("./version/web_static_{}.tgz".format(opt)):
+                return os.path.abspath("./version/web_static_{}.tgz".format(opt))
+        else:
+                return None
 
 if __name__ == '__main__':
         do_pack()
