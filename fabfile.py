0e3d31f7d7b89f183d8953ee5e4bce33209c0d96
philimon-reset <philimonderib2019@gmail.com>
2021-08-26 21:39:09 +0000

    advanced

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,5 @@ env.user = 'ubuntu'
 env.hosts = ["104.196.155.240"]
 env.key_filename = "/home/ubuntu/.ssh/id_rsa"
 def hello():
-    target = run('ls | xargs stat | grep "Change" | cut -d " " -f 2,3')
-    print(target[0])
-
+    target = run('ls ~/AirBnB_Clone_V2/versions | xargs stat | grep "Change" | cut -d " " -f 2,3 > ./text')
+    
