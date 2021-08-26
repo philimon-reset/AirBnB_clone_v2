464b6849d1143ca24baefe1070875fabebc88f26
Philimon Derib <philimonderib2019@gmail.com>
2021-08-27 01:13:26 +0300

    test

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,6 @@ env.user = 'ubuntu'
 env.hosts = ["104.196.155.240"]
 env.key_filename = "/home/ubuntu/.ssh/id_rsa"
 def hello():
-    target = run("ls -t .")
     with cd("./versions"):
-        target = run('ls -t')
+        target = run('ls -t .')
         print(target.split())
