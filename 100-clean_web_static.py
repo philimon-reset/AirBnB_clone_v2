98474fe8a5d76c6c4b09a1422dec4dd91cc04ce7
Philimon Derib <philimonderib2019@gmail.com>
2021-08-26 23:57:27 +0300

    remove print

---
+++ 100-clean_web_static.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""web server distribution"""
+from fabric.api import *
+import os.path
+
+env.user = 'ubuntu'
+env.hosts = ["104.196.155.240", "34.74.146.120"]
+env.key_filename = "/home/ubuntu/.ssh/id_rsa"
+
+
+def do_clean(number=0):
+    """deletes out-of-date archives,
+    """
+    
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -29,22 +29,16 @@ def do_deploy(archive_path):
     """distributes an archive to your web servers
     """
     if os.path.exists(archive_path) is False:
-        print(archive_path)
         return False
     try:
-        print("arc1")
         arc = archive_path.split("/")
-        print("arc")
         base = arc[1].strip('.tgz')
         put(archive_path, '/tmp/')
         sudo('mkdir -p /data/web_static/releases/{}'.format(base))
-        print("arc2")
         main = "/data/web_static/releases/{}".format(base)
-        print("arc3")
         sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
         sudo('rm /tmp/{}'.format(arc[1]))
         sudo('mv {}/web_static/* {}/'.format(main, main))
-        print("arc5")
         sudo('rm -rf /data/web_static/current')
         sudo('ln -s {}/ "/data/web_static/current"'.format(main))
         return True
