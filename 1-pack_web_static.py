36cb3747d0b5ae31caa4cfe72fa39a1a97df45a3
Philimon Derib <philimonderib2019@gmail.com>
2021-08-26 13:10:50 +0300

    fab

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,10 +1,18 @@
 from fabric.api import local
 import tarfile
 import os.path
+import re
 from datetime import datetime
 
 def do_pack():
         target = local("mkdir -p version")
-        name = datetime.now()
-        with tarfile.open(str(name), "w:gz") as tar:
-                tar.add("web_static", arcname=os.path.basename('./version'))
+        name = str(datetime.now()).replace(" ", '')
+        opt = re.sub(r'[^\w\s]','', name)
+        with tarfile.open(opt, "w:gz") as tar:
+                tar.add('web_static', arcname=os.path.basename('./version'))
+        t = tarfile.open(opt, 'r')
+        for member in t.getmembers():
+                print(member.name)
+
+if __name__ == '__main__':
+        do_pack()
