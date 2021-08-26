83b7b2c9a94f37061b58c2478e79814c218d27d7
Philimon Derib <philimonderib2019@gmail.com>
2021-08-27 01:57:55 +0300

    pep8

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -10,11 +10,12 @@ env.key_filename = "/home/ubuntu/.ssh/id_rsa"
 
 def do_clean(number=0):
     """deletes out-of-date archives"""
-    target = sudo('ls -t ./AirBnB_Clone_V2/versions/').split()
+    target = local('ls -t ./versions/').split()
     print(len(target), number, target[1])
     if len(target) == 2:
             if number == 0 or number == 1:
                     local('rm -f ./versions/{}'.format(target[1]))
+                    print("here")
                     sudo('rm -rf /data/web_static/releases/{}'.format(target[1].strip(".tgz")))
             elif number == 2:
                     pass
@@ -28,7 +29,6 @@ def do_clean(number=0):
                 cl = target[2:]
                 for i in range(len(cl)):
                         local('rm -f ./versions/{}'.format(cl[i]))
-                        print(sudo("pwd"))
                         sudo('rm -rf /data/web_static/releases/{}'.format(cl[i].strip(".tgz")))
     else:
             pass
