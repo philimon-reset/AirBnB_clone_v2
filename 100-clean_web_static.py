d4b427bcb3910dd61a9c3412781bddcaf652c655
Philimon Derib <philimonderib2019@gmail.com>
2021-08-27 01:46:33 +0300

    pep8

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -13,15 +13,16 @@ def do_clean(number=0):
     target = sudo('ls -t ./AirBnB_Clone_V2/versions/').split()
     if len(target) > 1:
         if number == 0 or number == 1:
-                number = 1
+                cl = target[1:]
+                for i in range(len(cl)):
+                        local('rm -f ./versions/{}'.format(cl[i]))
+                        print(sudo("pwd"))
+                        sudo('rm -rf /data/web_static/releases/{}'.format(cl[i].strip(".tgz")))
         elif number == 2:
-                number = 2
-        cl = target[number:]
-        print(cl)
-        print(number)
-        for i in range(len(cl)):
-                local('rm -f ./versions/{}'.format(cl[i]))
-                print(sudo("pwd"))
-                sudo('rm -rf /data/web_static/releases/{}'.format(cl[i].strip(".tgz")))
+                cl = target[2:]
+                for i in range(len(cl)):
+                        local('rm -f ./versions/{}'.format(cl[i]))
+                        print(sudo("pwd"))
+                        sudo('rm -rf /data/web_static/releases/{}'.format(cl[i].strip(".tgz")))
     else:
             pass
