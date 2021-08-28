13853e488a94818bcc61b9e3963d0d025d22707d
Philimon Derib <philimonderib2019@gmail.com>
2021-08-28 19:54:19 +0300

    importing connections

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """web server distribution"""
 from fabric.api import *
-from fabric.connection import Connection
+from fabric.state import commands, connections
 import os.path
 
 env.user = 'ubuntu'
@@ -11,7 +11,7 @@ env.key_filename = "~/id_rsa"
 
 def do_clean(number=0):
     """deletes out-of-date archives"""
-    with Connection('root@localhost') as cn:
+    with connections('root@localhost') as cn:
         target = cn.sudo('ls -t ~/AirBnB_Clone_V2/versions/').split()
     with cd("/data/web_static/releases"):
         target_R = sudo("ls -t .").split()
