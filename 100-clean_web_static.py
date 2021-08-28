01eadc58b4e0afaf8986eca906751afc97d50c06
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-08-28 15:13:28 +0300

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -10,12 +10,12 @@ env.key_filename = "/home/ubuntu/.ssh/id_rsa"
 
 def do_clean(number=0):
     """deletes out-of-date archives"""
-    target = local('ls -t ./AirBnB_Clone_V2/versions/').split()
+    target = local('ls -t ~/AirBnB_Clone_V2/versions/').split()
     target_R = sudo('ls -t /data/web_static/releases/').split()
     paths = "/data/web_static/releases"
     if len(target) == 2:
         if number == '0' or number == '1':
-            local('rm -f ./AirBnB_Clone_V2/versions/{}'.format(target[-1]))
+            local('rm -f ~/AirBnB_Clone_V2/versions/{}'.format(target[-1]))
             sudo('rm -rf {}/{}'.format(paths, target_R[-1].strip(".tgz")))
         elif number == '2':
             pass
@@ -24,14 +24,14 @@ def do_clean(number=0):
             cl = target[1:]
             rem = target_R[1:]
             for i in range(len(cl)):
-                local('rm -f ./AirBnB_Clone_V2/versions/{}'.format(target[-1]))
+                local('rm -f ~/AirBnB_Clone_V2/versions/{}'.format(target[-1]))
             for j in range(len(rem)):
                 sudo('rm -rf {}/{}'.format(paths, rem[-1].strip(".tgz")))
         elif number == '2':
             cl = target[2:]
             rem = target_R[2:]
             for i in range(len(cl)):
-                local('rm -f ./AirBnB_Clone_V2/versions/{}'.format(target[-1]))
+                local('rm -f ~/AirBnB_Clone_V2/versions/{}'.format(target[-1]))
             for j in range(len(rem)):
                 sudo(
                     'rm -rf {}/{}'.format(paths, rem[-1].strip(".tgz")))
