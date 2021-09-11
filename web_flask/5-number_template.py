5c85698cadf27982f66eecdb7b90c0f1343b0317
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-09-11 23:26:48 +0300

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -41,7 +41,7 @@ def num_display(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def render_num_route(n):
         """serves static files"""
-        return render_template("5-number.html", my_num=n)
+        return render_template("5-number.html", name=n)
 
 
 if __name__ == "__main__":
