c6754f5a80bec4c52a58d0694a3e1901c91f07c9
philimon-reset <philimonderib2019@gmail.com>
2021-09-11 07:14:34 +0300

    pep

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Flask framework
+    """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run()
