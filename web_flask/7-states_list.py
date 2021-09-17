be8663295854afe152606ee607720112dbcf44e5
philimon-reset <philimonderib2019@gmail.com>
2021-09-18 00:46:51 +0300

    test #4

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Flask framework
 """
-from flask import Flask, url_for, render_template, request
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 
